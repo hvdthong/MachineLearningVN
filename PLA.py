1db2c9e650ca173eb4d1567c6997c1c725211df2
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2017-07-01 04:14:50 +0800

    modify PLA

--- PLA.py
+++ PLA.py
@@ -59,48 +59,48 @@ print len(w)
 # print(w)
 # print(len(w))
 
-# def draw_line(w):
-#     w0, w1, w2 = w[0], w[1], w[2]
-#     if w2 != 0:
-#         x11, x12 = -100, 100
-#         return plt.plot([x11, x12], [-(w1*x11 + w0)/w2, -(w1*x12 + w0)/w2], 'k')
-#     else:
-#         x10 = -w0/w1
-#         return plt.plot([x10, x10], [-100, 100], 'k')
+def draw_line(w):
+    w0, w1, w2 = w[0], w[1], w[2]
+    if w2 != 0:
+        x11, x12 = -100, 100
+        return plt.plot([x11, x12], [-(w1*x11 + w0)/w2, -(w1*x12 + w0)/w2], 'k')
+    else:
+        x10 = -w0/w1
+        return plt.plot([x10, x10], [-100, 100], 'k')
 
 
-## Visualization
-# import matplotlib.animation as animation
-# from matplotlib.animation import FuncAnimation
+# Visualization
+import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
 
-# def viz_alg_1d_2(w):
-#     it = len(w)
-#     fig, ax = plt.subplots(figsize=(5, 5))
+def viz_alg_1d_2(w):
+    it = len(w)
+    fig, ax = plt.subplots(figsize=(5, 5))
 
-#     def update(i):
-#         ani = plt.cla()
-#         #points
-#         ani = plt.plot(X0[0, :], X0[1, :], 'b^', markersize = 8, alpha = .8)
-#         ani = plt.plot(X1[0, :], X1[1, :], 'ro', markersize = 8, alpha = .8)
-#         ani = plt.axis([0 , 6, -2, 4])
-#         i2 =  i if i < it else it-1
-#         ani = draw_line(w[i2])
-#         if i < it-1:
-#             # draw one  misclassified point
-#             circle = plt.Circle((X[1, m[i]], X[2, m[i]]), 0.15, color='k', fill = False)
-#             ax.add_artist(circle)
-#         # hide axis
-#         cur_axes = plt.gca()
-#         cur_axes.axes.get_xaxis().set_ticks([])
-#         cur_axes.axes.get_yaxis().set_ticks([])
+    def update(i):
+        ani = plt.cla()
+        #points
+        ani = plt.plot(X0[0, :], X0[1, :], 'b^', markersize = 8, alpha = .8)
+        ani = plt.plot(X1[0, :], X1[1, :], 'ro', markersize = 8, alpha = .8)
+        ani = plt.axis([0 , 6, -2, 4])
+        i2 =  i if i < it else it-1
+        ani = draw_line(w[i2])
+        if i < it-1:
+            # draw one  misclassified point
+            circle = plt.Circle((X[1, m[i]], X[2, m[i]]), 0.15, color='k', fill = False)
+            ax.add_artist(circle)
+        # hide axis
+        cur_axes = plt.gca()
+        cur_axes.axes.get_xaxis().set_ticks([])
+        cur_axes.axes.get_yaxis().set_ticks([])
 
-#         label = 'PLA: iter %d/%d' %(i2, it-1)
-#         ax.set_xlabel(label)
-#         return ani, ax
+        label = 'PLA: iter %d/%d' %(i2, it-1)
+        ax.set_xlabel(label)
+        return ani, ax
 
-#     anim = FuncAnimation(fig, update, frames=np.arange(0, it + 2), interval=1000)
-#     # save
-#     anim.save('pla_vis.gif', dpi = 100, writer = 'imagemagick')
-#     plt.show()
+    anim = FuncAnimation(fig, update, frames=np.arange(0, it + 2), interval=1000)
+    # save
+    # anim.save('pla_vis.gif', dpi = 100, writer = 'imagemagick')
+    plt.show()
 
-# viz_alg_1d_2(w)
+viz_alg_1d_2(w)
