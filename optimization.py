dd4facf752cd95c38f9111f461cd903d0f1413aa
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2017-09-28 20:29:28 +0800

    adding optimization and SVM

--- optimization.py
+++ optimization.py
@@ -1,1 +1,22 @@
-print 'hello'
+from cvxopt import matrix, solvers
+c = matrix([-5., -3.])
+G = matrix([[1., 2., 1., -1., 0.], [1., 1., 4., 0., -1.]])
+h = matrix([10., 16., 32., 0., 0.])
+
+solvers.options['show_progress'] = False
+sol = solvers.lp(c, G, h)
+
+print('Solution"')
+print(sol['x'])
+
+
+P = matrix([[1., 0.], [0., 1.]])
+q = matrix([-10., -10.])
+G = matrix([[1., 2., 1., -1., 0.], [1., 1., 4., 0., -1.]])
+h = matrix([10., 16., 32., 0., 0])
+
+solvers.options['show_progress'] = False
+sol = solvers.qp(P, q, G, h)
+
+print('Solution:')
+print(sol['x'])
